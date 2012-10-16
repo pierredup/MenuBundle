aabccff60c989a573cb8f74894f177281be1285b
Pierre du Plessis <pierre@pcservice.co.za>
2012-10-16 11:19:09 +0200

    Updated License

--- Event/ConfigureMenuEvent.php
+++ Event/ConfigureMenuEvent.php
@@ -3,7 +3,10 @@
 /*
  * This file is part of the CSMenuBundle package.
  *
- * (c) Pierre du Plessis <info@customscripts.co.za>
+ * (c) Pierre du Plessis <info@customscripts.co.za> / CustomScripts
+ *
+ * @author Pierre du Plessis <info@customscripts.co.za>
+ * @link(https://www.github.com/CustomScripts/MenuBundle)
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
